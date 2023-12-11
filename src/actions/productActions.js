cca93299a51bad6feac1142caf311cc5b3aaf703
danio86 <d.harder@gmx.at>
2023-12-11 18:29:26 +0000

    changed get products command in product

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -15,7 +15,10 @@ export const listProducts = (keyword = '') => async (dispatch) => {
     try {
         dispatch({ type: PRODUCT_LIST_REQUEST })
 
-        const { data } = await axios.get(`/api/products?keyword=${keyword}`)
+        // const { data } = await axios.get(`/api/products?keyword=${keyword}`)
+
+        const { data } = await axios.get(`https://proclimbers-backend-d69c858b50d1.herokuapp.com/api/products?keyword=${keyword}`);
+
 
         console.log(data, 'test') // Log the response data
 
