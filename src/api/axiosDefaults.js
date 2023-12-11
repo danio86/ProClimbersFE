9b2b024378feceb3162fc2dbf87e6ec1cf2ecf7f
danio86 <d.harder@gmx.at>
2023-12-11 13:05:32 +0000

    deleted old api file

--- src/api/axiosDefaults.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-axios.defaults.baseURL = "https://proclimbers-backend-d69c858b50d1.herokuapp.com/";
-axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
-axios.defaults.withCredentials = true;
-
-/* axios intercepters catches api-user-response/request infos  */
-export const axiosReq = axios.create();
-export const axiosRes = axios.create();
