63fd6773fe4bac848b5f26abf3f37b67056500df
danio86 <d.harder@gmx.at>
2023-12-11 15:41:50 +0000

    add header link tests to NavBartests.js

--- src/components/__tests__/NavBar.test.js
+++ src/components/__tests__/NavBar.test.js
@@ -4,10 +4,17 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import Header from '../Header';
 
-// Create a mock store
-const store = createStore(() => ({ userLogin: { userInfo: null } }));
 
-test('renders learn react link', () => {
+
+const mockReducer = (state = { userLogin: { userInfo: { name: 'Username', isAdmin: false } } }, action) => state;
+
+
+// Create a mock store
+// const store = createStore(() => ({ userLogin: { userInfo: null } }));
+const store = createStore(mockReducer);
+
+
+test('renders header', () => {
     render( 
     <Provider store={store}>
         <BrowserRouter>
@@ -20,3 +27,37 @@ test('renders learn react link', () => {
     const linkElement = screen.getByText(/ProClimbing/i);
     expect(linkElement).toBeInTheDocument();
 });
+
+
+test('renders userdropdown if the user is logged in', async () => {
+    render( 
+    <Provider store={store}>
+        <BrowserRouter>
+            <Header />
+        </BrowserRouter>
+    </Provider>
+    );
+
+   const linkElement = screen.getByText(/Username/i);
+   expect(linkElement).toBeInTheDocument();
+});
+
+
+test('reders login link if the user is not logged in', async () => {
+    const store = createStore(() => ({ userLogin: { userInfo: null } }));
+    render( 
+    <Provider store={store}>
+        <BrowserRouter>
+            <Header />
+        </BrowserRouter>
+    </Provider>
+    );
+
+   const linkElement = screen.getByText(/LogIn/i);
+   expect(linkElement).toBeInTheDocument();
+}
+);
+
+
+
+
