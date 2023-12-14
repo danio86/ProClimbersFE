d8151063b76bb84795371854a8b3f01b3b7703bc
danio86 <d.harder@gmx.at>
2023-12-14 17:02:27 +0000

    add chenged localstorage back in store.js

--- src/store.js
+++ src/store.js
@@ -37,31 +37,31 @@ const reducer = combineReducers({
     productReview: productReviewReducer,
 })
 
-// const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
-// // we need to parse it because we stringify it when we store it in local storage
+const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+// we need to parse it because we stringify it when we store it in local storage
 
-// const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
-// // if there is a user info in local storage, we parse it, otherwise we set it to null
+const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
+// if there is a user info in local storage, we parse it, otherwise we set it to null
 
-// console.log('User info from storage:', userInfoFromStorage); // Add this line
-
-
-// const shippingAddressFromStorage = localStorage.getItem('shippingAddress') ? JSON.parse(localStorage.getItem('shippingAddress')) : {}
-
-const cartItemsFromStorage = typeof window !== 'undefined' && localStorage.getItem('cartItems') 
-    ? JSON.parse(localStorage.getItem('cartItems')) 
-    : []
-
-const userInfoFromStorage = typeof window !== 'undefined' && localStorage.getItem('userInfo') 
-    ? JSON.parse(localStorage.getItem('userInfo')) 
-    : null
-
-const shippingAddressFromStorage = typeof window !== 'undefined' && localStorage.getItem('shippingAddress') 
-    ? JSON.parse(localStorage.getItem('shippingAddress')) 
-    : {}
+console.log('User info from storage:', userInfoFromStorage); // Add this line
 
 
-    
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress') ? JSON.parse(localStorage.getItem('shippingAddress')) : {}
+
+// const cartItemsFromStorage = typeof window !== 'undefined' && localStorage.getItem('cartItems') 
+//     ? JSON.parse(localStorage.getItem('cartItems')) 
+//     : []
+
+// const userInfoFromStorage = typeof window !== 'undefined' && localStorage.getItem('userInfo') 
+//     ? JSON.parse(localStorage.getItem('userInfo')) 
+//     : null
+
+// const shippingAddressFromStorage = typeof window !== 'undefined' && localStorage.getItem('shippingAddress') 
+//     ? JSON.parse(localStorage.getItem('shippingAddress')) 
+//     : {}
+
+
+
 const initialState = {
     cart: { cartItems: cartItemsFromStorage, shippingAddress: shippingAddressFromStorage }, // we need to pass in an object because our cartReducer expects an object
     userLogin: { userInfo: userInfoFromStorage }
@@ -70,7 +70,6 @@ const initialState = {
 const middleware = [thunk]
 
 const store = createStore(reducer, initialState , composeWithDevTools(applyMiddleware(...middleware)))
-// const store = configureStore(reducer, initialState , composeWithDevTools(applyMiddleware(...middleware)))
 
 
 export default store
